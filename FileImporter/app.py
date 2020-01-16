7b811488fbe16f6a0162c2d4c0b2f1fc795ce7d6
likan <likanucf@yahoo.fr>
2020-01-17 00:45:11 +0100

    Init structure

--- FileImporter/app.py
+++ FileImporter/app.py
@@ -2,9 +2,4 @@ from FileImporter.file.FileCompare import FileObject, FileContext, compare_file_
 
 
 def run():
-    ctx1 = FileContext()
-    ctx1.populate("/home/likan/test/rep1/")
-    ctx2 = FileContext()
-    ctx2.populate("/home/likan/test/rep2/")
-
-    compare_file_contexts(ctx1, ctx2)
+    pass
--- FileImporter/file/FileCompare.py
+++ FileImporter/file/FileCompare.py
@@ -16,36 +16,47 @@ class FileObject(object):
 
 
 class FileContext:
-    files: List[FileObject] = []
 
     def __init__(self) -> None:
+        self.files: List[FileObject] = []
         super().__init__()
 
     def populate(self, path: str):
         for filepath in iglob(path + '**/*', recursive=True):
-            if not ntpath.isdir(filepath) :
+            if not ntpath.isdir(filepath):
                 filestat = os.stat(filepath)
                 f = FileObject(filepath, ntpath.basename(filepath), filestat.st_size)
-                f.print_info()
                 self.files.append(f)
+
+    def print_info(self):
+        for f in self.files:
+            f.print_info()
+
+    def add_file(self, f: FileObject):
+        self.files.append(f)
+
+    def has_file(self, f: FileObject):
+        for f_dest in self.files:
+            found = compare_file_objects(f, f_dest)
+            if found:
+                return found
+        return found
 
 
 def compare_file_objects(f1: FileObject, f2: FileObject):
     return f1.filename == f2.filename and f1.size == f2.size
 
 
+def compare_file_objects_strict(f1: FileObject, f2: FileObject):
+    return f1.filename == f2.filename and f1.size == f2.size and f1.filepath == f2.filepath
+
+
 def compare_file_contexts(fctx1: FileContext, fctx2: FileContext):
-    files_not_found: List[FileObject] = []
+    files_not_found_context = FileContext()
 
     for fOrigin in fctx1.files:
-        found: bool = False
-        for fDest in fctx2.files:
-            found = compare_file_objects(fOrigin, fDest)
+        found: bool = fctx2.has_file(fOrigin)
         if not found:
-            files_not_found.append(fOrigin)
-            print("NOT FOUND :")
-            fOrigin.print_info()
+            files_not_found_context.add_file(fOrigin)
 
-    return files_not_found
-
-
+    return files_not_found_context
